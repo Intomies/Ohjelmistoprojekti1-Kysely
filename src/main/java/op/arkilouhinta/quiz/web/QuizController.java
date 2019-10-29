ea6c755936bec00920ab7f6b662ef4563c565580
HeiniRa <46703786+HeiniRa@users.noreply.github.com>
2019-10-29 15:14:35 +0200

    Controller added

---
+++ src/main/java/op/arkilouhinta/quiz/web/QuizController.java
@@ -0,0 +1,32 @@
+package op.arkilouhinta.quiz.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import op.arkilouhinta.quiz.domain.QuestionaireRepository;
+
+@Controller
+public class QuizController {
+	
+	@Autowired
+	private QuestionaireRepository repository;
+	
+	//Front page
+	@GetMapping
+	public String index() {
+		return "/";
+	}
+	
+	//Show all quoestionnaires in Thymeleaf template
+	@GetMapping
+	public String questionnares(Model model) {
+		model.addAttribute("quoeastionnaires", repository.findAll());
+		return "quoestionnaires";
+	}
+	
+	
+	
+
+}
