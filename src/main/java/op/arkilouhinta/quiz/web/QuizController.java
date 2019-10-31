3a92c7e6e832956550aee6c61daf7ff57f53047b
Hartsa <harrinupponen@gmail.com>
2019-10-31 13:37:52 +0200

    Fixed again

--- src/main/java/op/arkilouhinta/quiz/web/QuizController.java
+++ src/main/java/op/arkilouhinta/quiz/web/QuizController.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-<<<<<<< HEAD
-=======
 import op.arkilouhinta.quiz.domain.Questionnaire;
->>>>>>> 00807b1db684d845e6a496f1a2763b4ac0287120
+
 import op.arkilouhinta.quiz.domain.QuestionnaireRepository;
 
 @Controller
@@ -26,19 +24,11 @@ public class QuizController {
 	}
 	
 	//Show all quoestionnaires in Thymeleaf template
-<<<<<<< HEAD
 	@GetMapping("/questionnaires")
 	public String questionnaires(Model model) {
 		model.addAttribute("questionnaires", repository.findAll());
 		return "questionnaires";
-=======
-	@GetMapping("quoestionnaires")
-	public String questionnares(Model model) {
-		model.addAttribute("quoeastionnaires", repository.findAll());
-		return "questionnaires";
->>>>>>> 00807b1db684d845e6a496f1a2763b4ac0287120
 	}
-	
 	
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
 	public String save(Questionnaire questionnaire) {
