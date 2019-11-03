0ab266daef023b1fa212664b3ca233c3e2e2a327
Intomies <valtteri.nummi@gmail.com>
2019-11-03 14:09:33 +0200

    added smoketest for controller class and a couple of junit tests to QuestionRepo and QuestionnaireRepo

--- src/test/java/op/arkilouhinta/quiz/QuizApplicationTests.java
+++ src/test/java/op/arkilouhinta/quiz/QuizApplicationTests.java
@@ -1,14 +1,27 @@
 package op.arkilouhinta.quiz;
 
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import op.arkilouhinta.quiz.web.QuizController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class QuizApplicationTests {
+public class QuizApplicationTests {
+	
+	@Autowired
+	private QuizController controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
