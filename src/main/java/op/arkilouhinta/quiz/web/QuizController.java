c895484979cb14659cc8107032b3c4f42b7f2da4
Intomies <valtteri.nummi@gmail.com>
2019-11-05 11:37:50 +0200

    Added a comment considering restful adding of a new question

--- src/main/java/op/arkilouhinta/quiz/web/QuizController.java
+++ src/main/java/op/arkilouhinta/quiz/web/QuizController.java
@@ -20,91 +20,94 @@ import op.arkilouhinta.quiz.domain.QuestionnaireRepository;
 
 @Controller
 public class QuizController {
-	
+
 	@Autowired
 	private QuestionnaireRepository quizRepo;
-	
-	@Autowired private QuestionRepository questionRepo;
-	
-	//Front page
+
+	@Autowired
+	private QuestionRepository questionRepo;
+
+	// Front page
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	
-	//Show all questionnaires in Thymeleaf template
+
+	// Show all questionnaires in Thymeleaf template
 	@GetMapping("/questionnairelist")
 	public String getQuestionnaires(Model model) {
 		model.addAttribute("questionnaires", quizRepo.findAll());
 		return "questionnairelist";
 	}
-	
-	//Find all questions that are in question repository
+
+	// Find all questions that are in question repository
 	@GetMapping("/questionlist")
 	public String getQuestions(Model model) {
 		model.addAttribute("questions", questionRepo.findAll());
 		return "questionlist";
 	}
-	
-	//Empty form for adding a new questionnaire
+
+	// Empty form for adding a new questionnaire
 	@GetMapping("/addquestionnaire")
 	public String getEmptyQuizForm(Model model) {
 		model.addAttribute("quiz", new Questionnaire());
 		return "addquestionnaire";
 	}
- 	
-	//Add new quiz
+
+	// Add new quiz
 	@PostMapping("/addquestionnaire")
 	public String saveQuestionnaire(@ModelAttribute Questionnaire questionnaire) {
 		quizRepo.save(questionnaire);
 		return "questionnairelist";
 	}
-	
-	//Empty form for adding a new question
+
+	// Empty form for adding a new question
 	@GetMapping("/addquestion")
 	public String getEmptyQuestionForm(Model model) {
 		model.addAttribute("question", new Question());
 		model.addAttribute("quizs", quizRepo.findAll());
 		return "addquestion";
 	}
-	
-	//Add new question
+
+	// Add new question
 	@PostMapping("/addquestion")
 	public String saveQuestion(Question question) {
 		questionRepo.save(question);
 		return "redirect:/questionlist";
 	}
-	
-	//---REST METHODS---
-	
-	//Restful get all questionnaires
+
+	// ---REST METHODS---
+
+	// Restful get all questionnaires
 	@GetMapping("/questionnaires")
 	public @ResponseBody List<Questionnaire> getQuizsRest() {
 		return (List<Questionnaire>) quizRepo.findAll();
 	}
-	
-	//Restful get all questions
+
+	// Restful get all questions
 	@GetMapping("/questions")
 	public @ResponseBody List<Question> getQuestionsRest() {
 		return (List<Question>) questionRepo.findAll();
 	}
-	
-	//Restful get questionnaire by id
+
+	// Restful get questionnaire by id
 	@GetMapping("questionnaire/{id}")
-	public @ResponseBody Optional<Questionnaire> getQuizRest(@PathVariable("id") Long quizId ) {
+	public @ResponseBody Optional<Questionnaire> getQuizRest(@PathVariable("id") Long quizId) {
 		return quizRepo.findById(quizId);
 	}
-	
-	//Restful get question by id
+
+	// Restful get question by id
 	@GetMapping("question/{id}")
-	public @ResponseBody Optional<Question> getQuestionRest(@PathVariable("id") Long questionId ) {
+	public @ResponseBody Optional<Question> getQuestionRest(@PathVariable("id") Long questionId) {
 		return questionRepo.findById(questionId);
 	}
-	
-	//Restful save new questionnaire
+
+	// Restful save new questionnaire
 	@PostMapping("/questionnaires")
 	public @ResponseBody Questionnaire saveQuizRest(@RequestBody Questionnaire quiz) {
 		return quizRepo.save(quiz);
 	}
+
+	// Restful save new question? How do we get {questionnaire id} into this method?
 
 }
