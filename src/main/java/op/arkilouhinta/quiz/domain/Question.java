0f2e810488736ae3d487de61901df11b4b762483
Intomies <valtteri.nummi@gmail.com>
2019-11-05 21:41:15 +0200

    Took Lombok out of objects and wrote constructors, getters, setters and toStrings to them manually

--- src/main/java/op/arkilouhinta/quiz/domain/Question.java
+++ src/main/java/op/arkilouhinta/quiz/domain/Question.java
@@ -9,15 +9,7 @@ import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Question {
 	
 	@Id
@@ -29,11 +21,56 @@ public class Question {
 	@JsonIgnore
 	@JoinColumn(name="questionnaireId")
 	private Questionnaire questionnaire;
+	
+	
+
+	public Question() {
+		super();
+	}
 
 	public Question(String question, Questionnaire questionnaire) {
 		super();
 		this.questionText = question;
 		this.questionnaire = questionnaire;
 	}
+	
+	public Question(Long questionId, String question, Questionnaire questionnaire) {
+		super();
+		this.questionId = questionId;
+		this.questionText = question;
+		this.questionnaire = questionnaire;
+	}
+
+	public Long getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(Long questionId) {
+		this.questionId = questionId;
+	}
+
+	public String getQuestionText() {
+		return questionText;
+	}
+
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+
+	public Questionnaire getQuestionnaire() {
+		return questionnaire;
+	}
+
+	public void setQuestionnaire(Questionnaire questionnaire) {
+		this.questionnaire = questionnaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [questionId=" + questionId + ", questionText=" + questionText + ", questionnaire="
+				+ this.getQuestionnaire() + "]";
+	}
+	
+	
 	
 }
