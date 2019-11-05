1dd2357686304bc6b6c952acc3fefbfd5f35735c
Intomies <valtteri.nummi@gmail.com>
2019-11-05 11:01:14 +0200

    Fixed controller methods

--- src/main/java/op/arkilouhinta/quiz/web/QuizController.java
+++ src/main/java/op/arkilouhinta/quiz/web/QuizController.java
@@ -4,39 +4,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import op.arkilouhinta.quiz.domain.Question;
+import op.arkilouhinta.quiz.domain.QuestionRepository;
 import op.arkilouhinta.quiz.domain.Questionnaire;
-
 import op.arkilouhinta.quiz.domain.QuestionnaireRepository;
 
 @Controller
 public class QuizController {
 	
 	@Autowired
-	private QuestionnaireRepository repository;
+	private QuestionnaireRepository quizRepo;
+	
+	@Autowired private QuestionRepository questionRepo;
 	
 	//Front page
-
-
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 	
-	//Show all quoestionnaires in Thymeleaf template
-	@GetMapping("/questionnaires")
-	public String questionnaires(Model model) {
-		model.addAttribute("questionnaires", repository.findAll());
-		return "questionnaires";
+	//Show all questionnaires in Thymeleaf template
+	@GetMapping("/questionnairelist")
+	public String getQuestionnaires(Model model) {
+		model.addAttribute("questionnaires", quizRepo.findAll());
+		return "questionnairelist";
 	}
 	
-	@RequestMapping(value = "/post", method = RequestMethod.POST)
-	public String save(Questionnaire questionnaire) {
-		repository.save(questionnaire);
-		return "questionnaires:";
+	//Find all questions that are in question repository
+	@GetMapping("/questionlist")
+	public String getQuestions(Model model) {
+		model.addAttribute("questions", questionRepo.findAll());
+		return "questionlist";
 	}
+	
+	//Add new quiz
+	@PostMapping("/addquestionnaire")
+	public String saveQuestionnaire(@ModelAttribute Questionnaire questionnaire) {
+		quizRepo.save(questionnaire);
+		return "questionnairelist";
+	}
+	
+	//Add new question
+	@PostMapping("/addquestion")
+	public String saveQuestion(Question question) {
+		questionRepo.save(question);
+		return "redirect:/questionlist";
+	}
+	
+	//---REST METHODS---
 	
 	
 
