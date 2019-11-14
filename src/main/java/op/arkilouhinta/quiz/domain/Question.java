53e5c3ff3a2f50fb8f7f35f7c129cb493ca5ccdf
Intomies <valtteri.nummi@gmail.com>
2019-11-14 12:18:45 +0200

    Added answerList to Question-class and changed rest home page to be alittle shorter

--- src/main/java/op/arkilouhinta/quiz/domain/Question.java
+++ src/main/java/op/arkilouhinta/quiz/domain/Question.java
@@ -1,11 +1,15 @@
 package op.arkilouhinta.quiz.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -30,6 +34,9 @@ public class Question {
 	@JsonIgnore
 	@JoinColumn(name="questionnaireId")
 	private Questionnaire questionnaire;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="question")
+	private List<Answer> answerList;
 	
 	public Question(String questionText, Questionnaire questionnaire) {
 		super();
